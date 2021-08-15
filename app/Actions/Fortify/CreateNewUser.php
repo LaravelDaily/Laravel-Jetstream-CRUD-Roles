793f1042366e4dd1654288bd43ef0d9189a4f77e
PovilasKorop <povilask007@gmail.com>
2021-08-15 07:46:41 +0300

    Fix bug with attaching roles

--- app/Actions/Fortify/CreateNewUser.php
+++ app/Actions/Fortify/CreateNewUser.php
@@ -31,7 +31,7 @@ class CreateNewUser implements CreatesNewUsers
             'password' => Hash::make($input['password']),
         ]);
 
-        $user->roles->attach(2);
+        $user->roles()->attach(2);
         return $user;
     }
 }
